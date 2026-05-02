538be02ab4ef2c54369b30c9805548ec5f4fe6b6
Ranvijay Tiwari <rt7999675@gmail.com>
2026-05-03 00:48:25 +0530

    Update firebase config to use env vars

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "interviewiq-ba6ba.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "interviewiq-ba6ba",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "interviewiq-ba6ba.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "862159592601",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:862159592601:web:7308d702cd708076ddec08"
 };
 
 const app = initializeApp(firebaseConfig);
