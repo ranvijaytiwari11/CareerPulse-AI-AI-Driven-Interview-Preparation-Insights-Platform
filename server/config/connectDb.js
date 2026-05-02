f30eed038f3b8b6bae7a3400fe84ea986835b2e2
Ranvijay Tiwari <rt7999675@gmail.com>
2026-05-03 04:19:16 +0530

    Fix: Add Vercel global CORS headers and prevent DB crash to fix Vercel 500 CORS Error

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -7,7 +7,6 @@ const connectDb = async () => {
     } catch (error) {
         console.error(`❌ DataBase Connection Failed: ${error.message}`)
         console.error("Full error:", error)
-        process.exit(1)
     }
 }
 
