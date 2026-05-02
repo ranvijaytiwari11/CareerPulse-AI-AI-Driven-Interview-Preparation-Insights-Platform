3fb6d17bd81a0b0263d90a8dd5aff9dc96a5851b
Ranvijay Tiwari <rt7999675@gmail.com>
2026-05-03 01:07:47 +0530

    Update firebase config with new project details

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "interviewiq-ba6ba.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "interviewiq-ba6ba",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "862159592601",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:862159592601:web:7308d702cd708076ddec08"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY || "AIzaSyCVfJw_NHaKbh5sQTkthTE0A_KDg69AKv4",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "careerpulse-ai-4d26a.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "careerpulse-ai-4d26a",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "careerpulse-ai-4d26a.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1030445088985",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1030445088985:web:23a5185fbab36d9563bec8"
 };
 
 const app = initializeApp(firebaseConfig);
